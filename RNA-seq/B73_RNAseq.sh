36b347a42255b8ebaf58143601af1fa62d5336be
SunLab-NNU <sunlh@njnu.edu.cn>
2025-10-05 19:43:47 +0800

    Add files via upload

---
+++ RNA-seq/B73_RNAseq.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#SBATCH --job-name=B73_RNAseq
+#SBATCH --partition=m256Partition
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --error=test_%j.err
+#SBATCH --output=test_%j.out
+
+input="${1}"
+SAMPLE_NAME="${2}"
+
+
+for sample in $(find . -name "${2}*_R1.fq.gz")
+do
+
+ ID=$(basename ${sample} _R1.fq.gz)
+ FASTQ1=${sample}
+ FASTQ2=${ID}_R2.fq.gz
+ THREADS=40
+  
+ mkdir -p ${input}
+ PUT=./${input}
+ mkdir -p ${PUT}/${input}_${ID}
+ OUTPUT=${PUT}/${input}_${ID}
+ mkdir -p ${OUTPUT}/LOG
+ LOG=${OUTPUT}/LOG
+ 
+ fastp --in1 ${FASTQ1} --in2 ${FASTQ2}  \
+--out1 ${OUTPUT}/${ID}_R1.fp.gz \
+--out2 ${OUTPUT}/${ID}_R2.fp.gz \
+-j ${OUTPUT}/${ID}.json \
+-h ${OUTPUT}/${ID}.html \
+--thread ${THREADS} \
+--length_required 70
+
+CFq_gz1=${OUTPUT}/${ID}_R1.fp.gz
+CFq_gz2=${OUTPUT}/${ID}_R2.fp.gz
+
+STAR  --runMode alignReads \
+    --genomeDir /home/cxshnl/Data/geneome/B73.genome.index  \
+    --runThreadN ${THREADS}   \
+    --readFilesIn ${CFq_gz1} ${CFq_gz2} \
+    --outFileNamePrefix ${OUTPUT}/${ID}_STAR \
+    --readFilesCommand gunzip -c \
+    --outReadsUnmapped Fastx \
+    --outSAMtype BAM SortedByCoordinate \
+    --outSAMattributes NH HI NM MD AS XS \
+    --outSAMstrandField intronMotif \
+    --outFilterMultimapNmax 10 \
+    --outFilterMismatchNmax 2  \
+    --alignIntronMin 35  \
+    --alignIntronMax 2000 \
+    --outWigType wiggle \
+    --outWigNorm RPM > ${OUTPUT}/${ID}_STAR.log 2>&1
+	
+samtools index -@ ${THREADS} ${OUTPUT}/${ID}_STARAligned.sortedByCoord.out.bam
+BAM="${OUTPUT}/${ID}_STARAligned.sortedByCoord.out.bam"
+
+samtools view -@ ${THREADS} -h ${BAM} |grep -E '^@|NH:i:1' |samtools view -@ ${THREADS} -Sb - > ${OUTPUT}/${ID}_STAR_Uniq.bam
+sambamba index -t ${THREADS} ${OUTPUT}/${ID}_STAR_Uniq.bam
+
+samtools index  ${OUTPUT}/${ID}_STAR_Uniq.bam
+bamCoverage -b ${OUTPUT}/${ID}_STAR_Uniq.bam \
+             -o ${OUTPUT}/${ID}_bigwig.bw \
+             --minMappingQuality 30 \
+             --smoothLength 50
+~                                            
+done
+
