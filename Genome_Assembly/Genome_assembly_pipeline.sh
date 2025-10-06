ff8d523f17bfc000a35f36f7990c01e63e99ca93
SunLab-NNU <sunlh@njnu.edu.cn>
2025-10-06 11:50:27 +0800

    Update and rename Assemble_nohic.sh to Genome_assembly_pipeline.sh

--- Genome_Assembly/Genome_assembly_pipeline.sh
+++ Genome_Assembly/Genome_assembly_pipeline.sh
@@ -1,22 +1,10 @@
-#!/bin/bash
-#SBATCH --job-name=Assemble_nohic.sh
-#SBATCH --partition=tcum512c128Partition
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=52
-#SBATCH --error=test_%j.err
-#SBATCH --output=test_%j.out
-
-echo "激活Assembly虚拟环境"
-echo "将【要比对的基因组文件】和【Hifi测序的原始bam文件】移动到 ~/Assemble 目录下"
-echo "输入 sh this.sh 2XXXXX_species Control.fa sample.bam"
-
 date=$1
 Control_FA=$2
 query=$3
 
 ID=$(basename ${query} .bam)
 
-# 创建输出目录
+
 output_dir=${date}
 mkdir -p "${output_dir}"
 
@@ -30,11 +18,11 @@ cd ${output_dir}
 
 ragtag.py correct ../${Control_FA} ${ID}.fasta -t 52
 ragtag.py scaffold ../${Control_FA} ./ragtag_output/ragtag.correct.fasta -t 52
-##ragtag.py patch ../${Control_FA} ./ragtag_output/ragtag.scaffold.fasta -t 52
+
 
 seqkit grep -v -p "Chr0_RagTag" ./ragtag_output/ragtag.scaffold.fasta > filtered_ragtag.fasta
 seqkit replace -p "_RagTag" -r "" filtered_ragtag.fasta > renamed_ragtag.fasta
 seqkit sort -N renamed_ragtag.fasta > ${ID}_sorted.fa
 
-##ragtag.py patch ./ragtag_output/ragtag.scaffold.fasta scaffolds_FINAL.fasta -t 32
+
 perl /data/cxshnl/Assemble/NGenomeSyn/bin/GetTwoGenomeSyn.pl -InGenomeA ../${Control_FA} -InGenomeB ${ID}_sorted.fa -OutPrefix ${date}
