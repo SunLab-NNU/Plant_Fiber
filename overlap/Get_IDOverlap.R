3fa25fa6d3df8904507f47a3c8dce530415dbf7e
SunLab-NNU <sunlh@njnu.edu.cn>
2025-10-06 16:21:22 +0800

    Update Get_IDOverlap.R

--- overlap/Get_IDOverlap.R
+++ overlap/Get_IDOverlap.R
@@ -6,31 +6,31 @@ library(grid)
 args <- commandArgs(trailingOnly = TRUE)
 
 if(length(args) < 2){
-  stop("请提供两个 TXT 文件路径作为参数，例如：Rscript script.R file1.txt file2.txt")
+  stop("Rscript script.R file1.txt file2.txt")
 }
 
 file_A <- args[1]
 file_B <- args[2]
 
-# 读取两个 TXT 文件，假设每个文件只有一列基因 ID
+
 A <- unique(read.table(file_A, stringsAsFactors = FALSE)[,1])
 B <- unique(read.table(file_B, stringsAsFactors = FALSE)[,1])
 
-# 设置拟南芥基因总量
-genome_size <- 27000  # 可以根据实际基因数调整
 
-# 计算交集和各集合大小
+genome_size <- 27000 
+
+
 overlap_count <- length(intersect(A, B))
 total_A <- length(A)
 total_B <- length(B)
 
 overlap_genes <- intersect(A, B)
 print(overlap_genes)
-# 超几何检验 P 值
+
 p_val <- phyper(overlap_count - 1, total_A, genome_size - total_A, total_B, lower.tail = FALSE)
 p_val <- signif(p_val, 3)
 
-# 生成输出文件名
+
 get_filename_without_ext <- function(filepath) {
   fname <- basename(filepath)
   sub("\\.[^.]*$", "", fname)
@@ -40,7 +40,7 @@ fileA_name <- get_filename_without_ext(file_A)
 fileB_name <- get_filename_without_ext(file_B)
 pdf_file <- paste0(fileA_name, "_vs_", fileB_name, "_Venn_with_Pvalue.pdf")
 
-# 保存 PDF
+
 pdf(pdf_file, width = 10, height = 10)
 
 draw.pairwise.venn(
